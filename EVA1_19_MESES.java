7a6ac51a722a355cd8f5814a6f0e4f29c8acea3b
SAMANTA-ABIGAIL-SANCHEZ-JUAREZ <L24550353@chihuahua2.tecnm.mx>
2024-09-26 16:54:13 -0600

    Add files via upload

---
+++ EVA1_19_MESES.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_19_meses;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sanch
+ */
+public class EVA1_19_MESES {
+
+    public static void main(String[] args) {
+        int mes;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Mes correspondiente de (1-1):");
+         mes = captu.nextInt();
+         if(mes == 1){
+            System.out.println("Enero");
+        }else if(mes == 2){
+            System.out.println("Febrero");
+        }else if(mes == 3){
+            System.out.println("Marzo");
+        }else if(mes == 4){
+            System.out.println("Abril");
+        }else if(mes == 5){
+            System.out.println("Mayo");
+        }else if(mes == 6){
+            System.out.println("Junio");
+        }else if(mes == 7){
+            System.out.println("Julio");
+        }else if (mes == 8){
+            System.out.println("Agostp");
+        }else if(mes == 9){
+            System.out.println("Septiembre");
+            }else if(mes == 10){
+            System.out.println("Octubre");
+        }else if (mes == 11){
+            System.out.println("Noviembre");
+        }else if(mes == 12){
+            System.out.println("Diciembre");
+        }else {
+        
+    }
+}
+}
