ddd01642ee8304101965338b2e709928fc1fb00e
SAMANTA-ABIGAIL-SANCHEZ-JUAREZ <L24550353@chihuahua2.tecnm.mx>
2024-09-30 16:09:55 -0600

    Add files via upload

---
+++ EVA1_22_CALIFAS.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_22_califas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sanch
+ */
+public class EVA1_22_CALIFAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         int A,B = 0,C = 0,D = 0,F = 0;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("CALIFICACION(1-100):");
+         A = captu.nextInt();
+         if(A == 100){
+            System.out.println("B=89");
+        }else if(B == 89){
+            System.out.println("C=79");
+        }else if(C == 79){
+            System.out.println("D=69");
+        }else if(D == 69){
+            System.out.println("F=59");
+            }else if(F == 59){//seleccion opcional
+                 System.out.println("Numero no valido (deben ser entre 70 al 100):");
+                
+                
+            }
+            
+                
+            }
+    }
+
